0b4685cb0d9c4f5b628a3ec30cac38384a6862ae
nadersirio <nadersirio@gmail.com>
2022-04-08 22:39:36 -0300

    JavaScript utilizado

---
+++ js.js
@@ -0,0 +1,9 @@
+function button() {
+  var email = document.getElementById("submitEmail").value
+  if (email == 0) {
+    alert("Por favor, coloque um Email no campo abaixo.");
+    return;
+  } else {
+  return;
+}
+}
