9656aaaf1712c581111168a0328ed2d775b13208
nadersirio <nadersirio@gmail.com>
2022-04-12 00:17:18 -0300

    javascript utilizado

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function submitForm() {
   else if (!email == true) {
     console.log(submitEmail.value);
     exibirImg.innerHTML = '<img class="header_imgError"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><g fill="none" fill-rule="evenodd"><circle cx="12" cy="12" r="12" fill="#F96464"/><path fill="#FFF" fill-rule="nonzero" d="M13.256 6v9.056h-2V6h2zm-.944 12.464c-.384 0-.699-.104-.944-.312a1.027 1.027 0 0 1-.368-.824c0-.33.125-.608.376-.832.25-.224.563-.336.936-.336.373 0 .68.112.92.336.24.224.36.501.36.832 0 .341-.117.616-.352.824-.235.208-.544.312-.928.312z"/></g></svg>';
-    exibirText.innerHTML = '<p class="header_result"> Please provide a valid email </p>';
+    exibirText.innerHTML = '<h6 class="header_result"> Please provide a valid email </h6>';
     submitEmail.classList.add("borderError");
   }
   //submitEmail.innerHTML = 'class="header__form-input--email (outra class aleatória)"';
